70fd24e08f2a26a7064ff84a604e4e4b07695365
waaghals <patrick.stroef@gmail.com>
2013-05-08 17:31:48 +0200

    Tabs tegevoegd aan voorbeeld

--- Project_MIN04SOB/src/nl/avans/min04sob/mvcvoorbeeld/BoardDemoController.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/mvcvoorbeeld/BoardDemoController.java
@@ -1,5 +1,7 @@
 package nl.avans.min04sob.mvcvoorbeeld;
 
+import javax.swing.JTabbedPane;
+
 import nl.avans.min04sob.scrabble.core.CoreConstraint;
 import nl.avans.min04sob.scrabble.core.CoreController;
 import nl.avans.min04sob.scrabble.core.CoreWindow;
@@ -7,10 +9,15 @@ import nl.avans.min04sob.scrabble.core.CoreWindow;
 public class BoardDemoController extends CoreController {
 
 	public BoardDemoController(CoreWindow window) {
-		BoardPanel playBoard = new BoardPanel();
+		//BoardPanel playBoard = new BoardPanel();
 		
-		window.add(playBoard, new CoreConstraint(10, 10, 10, 0));
+		JTabbedPane testTabs = new JTabbedPane();
+		testTabs.add("Game 1", new BoardPanel());
+		testTabs.add("Game 2", new BoardPanel());
+		testTabs.add("Game 3", new BoardPanel());
+		
+		window.add(testTabs, new CoreConstraint(10, 10, 10, 0));
 		window.pack();
-		addView(playBoard);
+		//addView(playBoard);
 	}
 }
--- Project_MIN04SOB/src/nl/avans/min04sob/mvcvoorbeeld/BoardPanel.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/mvcvoorbeeld/BoardPanel.java
@@ -2,8 +2,10 @@ package nl.avans.min04sob.mvcvoorbeeld;
 
 import java.beans.PropertyChangeEvent;
 
+import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 import nl.avans.min04sob.scrabble.core.CoreConstraint;
 import nl.avans.min04sob.scrabble.core.CorePanel;
@@ -58,5 +60,13 @@ public class BoardPanel extends CorePanel {
 		// Add the table to a scrolling pane
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane, new CoreConstraint(10, 10, 0, 0));
+		
+		JTextField textField = new JTextField();
+		textField.setText("A B Q E H E F");
+		add(textField, new CoreConstraint(8, 1, 0, 11));
+		
+		JButton button = new JButton();
+		button.setText("Kut");
+		add(button, new CoreConstraint(1, 2, 9, 11));
 	}
 }
