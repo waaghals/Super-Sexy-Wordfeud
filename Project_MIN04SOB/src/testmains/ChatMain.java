4a7c01b69b4ece76cd31f99613568cc4bd9f5a14
japguele <japguele@student.avans.nl>
2013-05-10 01:17:34 +0200

    chat update

--- Project_MIN04SOB/src/testmains/ChatMain.java
+++ Project_MIN04SOB/src/testmains/ChatMain.java
@@ -10,7 +10,7 @@ public class ChatMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ChatController cc = new ChatController();
+		ChatController cc = new ChatController(1,10);
 
 	}
 
