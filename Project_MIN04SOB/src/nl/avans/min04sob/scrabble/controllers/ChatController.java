e95854e3102a1ae074915f77fc34495f427a5e44
japguele <japguele@student.avans.nl>
2013-05-10 01:39:37 +0200

    chat update

---
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/ChatController.java
@@ -0,0 +1,100 @@
+package nl.avans.min04sob.scrabble.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JFrame;
+
+import nl.avans.min04sob.scrabble.core.CoreController;
+import nl.avans.min04sob.scrabble.models.ChatModel;
+import nl.avans.min04sob.scrabble.views.ChatPanel;
+
+public class ChatController extends CoreController{
+private ChatPanel chatpanel;
+private ChatModel chatmodel;
+private JFrame frame;	
+private final int xsizechat, ysizechat , checkmessagestimer , player_id ,game_id;
+
+	public ChatController(int playerid,int gameid){
+		xsizechat = 150;
+		ysizechat = 250;
+		checkmessagestimer = 10;
+	
+		this.player_id = playerid;
+		this.game_id = gameid;
+		
+		chatpanel = new ChatPanel(xsizechat,ysizechat);	
+		chatmodel = new ChatModel(game_id,playerid);
+	
+	addView(chatpanel);
+	chatpanel.getChatfield().setText(chatpanel.getChatfield().getText() +chatmodel.allmessagesforstart());
+	this.startcheckingmessages();
+	
+	
+	
+	
+	
+	
+	chatpanel.addlistenerchatfield(new KeyListener(){
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+				send();
+				
+			}
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	});
+	chatpanel.addlistenerchatsendbutton(new ActionListener(){
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			send();
+			
+
+			}
+			
+		
+		
+	});
+}
+	public void send(){
+if (!chatpanel.getChatfieldsend().getText().equals("") && !chatpanel.getChatfieldsend().getText().equals(" ")) {
+			
+	chatpanel.getChatfield().setText(chatpanel.getChatfield().getText() + "you: " + chatpanel.getChatfieldsend().getText() + "\n");
+			chatmodel.send(chatpanel.getChatfieldsend().getText());
+			chatpanel.getChatfieldsend().setText("");
+		}
+	}
+	public void startcheckingmessages(){
+		ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+		exec.scheduleAtFixedRate(new Runnable() {
+		  @Override
+		  public void run() {
+		    chatpanel.getChatfield().setText(chatpanel.getChatfield().getText() +chatmodel.newmessages());
+		  }
+		}, 0, this.checkmessagestimer, TimeUnit.SECONDS);
+	}
+
+	public ChatPanel getchatpanel(){
+		return chatpanel;
+	}
+}
---
+++ Project_MIN04SOB/src/testmains/ChatMain.java
@@ -0,0 +1,19 @@
+package testmains;
+import nl.avans.min04sob.scrabble.controllers.ChatController;
+
+
+
+
+public class ChatMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		ChatController cc = new ChatController(1,10);
+
+
+	}
+
+}
