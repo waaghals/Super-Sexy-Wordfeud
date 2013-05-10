0084f4c7158a7f1a91ce38999eb21666c9e8f6d0
waaghals <patrick.stroef@gmail.com>
2013-05-10 15:24:43 +0200

    Added a game id

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/ChatandGamePanel.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/ChatandGamePanel.java
@@ -10,7 +10,7 @@ public class ChatandGamePanel extends JPanel{
 
 	public ChatandGamePanel(int playerid){
 		setPreferredSize(new Dimension(210, 300));
-		ChatController chat = new ChatController(playerid);
+		ChatController chat = new ChatController(playerid, 1);
 		add(chat.getchatpanel());
 	}
 }
