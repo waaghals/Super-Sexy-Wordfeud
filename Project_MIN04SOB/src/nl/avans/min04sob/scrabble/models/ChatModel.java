212222230dbba4df9d13432a6729599c9c82c10a
waaghals <patrick.stroef@gmail.com>
2013-05-10 15:23:32 +0200

    Added camelCase

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/ChatModel.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/ChatModel.java
@@ -10,59 +10,81 @@ import java.util.Date;
 import nl.avans.min04sob.scrabble.core.CoreModel;
 import nl.avans.min04sob.scrabble.core.Dbconnect;
 
-public class ChatModel extends CoreModel{
-private final int game_id , player_id;
-private String timelastmessage;
-	public ChatModel(int game_id , int player_id){
-		this.game_id = game_id;
-		this.player_id = player_id;
-		timelastmessage = "2000-1-1 00:00:00";
+public class ChatModel extends CoreModel {
+	private final int gameId, playerId;
+	private String timeLastMessage;
+
+	public ChatModel(int gameId, int playerId) {
+		this.gameId = gameId;
+		this.playerId = playerId;
+		timeLastMessage = "2000-1-1 00:00:00";
 	}
-	public void send(String newmessage){
-		
-		try{
+
+	public void send(String newmessage) {
+
+		try {
 			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			Date date = new Date();
 			String currentdate = dateFormat.format(date);
-			Dbconnect.getInstance().query("INSERT INTO chat(game_id,player_id,message,send_at) VALUES('"+this.game_id+"','"+this.player_id+"','"+newmessage+"','"+currentdate+"');");
-		}catch(SQLException sql){
-			
+			Dbconnect.getInstance().query(
+					"INSERT INTO chat(game_id,player_id,message,send_at) VALUES('"
+							+ this.gameId + "','" + this.playerId + "','"
+							+ newmessage + "','" + currentdate + "');");
+		} catch (SQLException sql) {
+			sql.printStackTrace();
 		}
 	}
-	public String newmessages(){
-		String returnnewmessages = "";
+
+	public String newmessages() {
+		String newMessages = "";
 		try {
-			ResultSet newmessages = Dbconnect.getInstance().select("SELECT player_id, message, send_at FROM chat WHERE game_id = "+this.game_id+" AND send_at > '"+this.timelastmessage+"' ORDER BY send_at ASC");
-			while(newmessages.next()){	
-				if(!(newmessages.getInt(1) == this.player_id)){
-					returnnewmessages = returnnewmessages + "oppenent : " + newmessages.getString(2)+"\n";
-				}	
-				timelastmessage = newmessages.getString(3);
+			ResultSet dbResult = Dbconnect.getInstance().select(
+					"SELECT player_id, message, send_at FROM chat WHERE game_id = "
+							+ this.gameId + " AND send_at > '"
+							+ this.timeLastMessage + "' ORDER BY send_at ASC");
+			while (dbResult.next()) {
+				if (!(dbResult.getInt(1) == this.playerId)) {
+					newMessages = newMessages + "oppenent : "
+							+ dbResult.getString(2) + "\n";
+				}
+				timeLastMessage = dbResult.getString(3);
 			}
-			return returnnewmessages;			
+			return newMessages;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return "error no connection to database.";
-		}		
+		}
 	}
-	public String allmessagesforstart(){
 
-		String returnnewmessages = "";
+	public String allMessagesFromStart() {
+
+		String newMessages = "";
 		try {
-			ResultSet newmessages = Dbconnect.getInstance().select("SELECT player_id, message, send_at FROM chat WHERE game_id = "+this.game_id+" AND send_at > '"+this.timelastmessage+"' ORDER BY send_at ASC");
-			while(newmessages.next()){	
-				if(newmessages.getInt(1) == this.player_id){
-					returnnewmessages = returnnewmessages + "you : " + newmessages.getString(2)+"\n";
-				}else{
-					returnnewmessages = returnnewmessages + "oppenent : " + newmessages.getString(2)+"\n";
+			ResultSet dbResult = Dbconnect.getInstance().select(
+					"SELECT player_id, message, send_at FROM chat WHERE game_id = "
+							+ this.gameId + " AND send_at > '"
+							+ this.timeLastMessage + "' ORDER BY send_at ASC");
+			while (dbResult.next()) {
+				if (dbResult.getInt(1) == this.playerId) {
+					newMessages = newMessages + "you : "
+							+ dbResult.getString(2) + "\n";
+				} else {
+					newMessages = newMessages + "oppenent : "
+							+ dbResult.getString(2) + "\n";
 				}
-				timelastmessage = newmessages.getString(3);
+				timeLastMessage = dbResult.getString(3);
 			}
-			return returnnewmessages;			
+			return newMessages;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return "error no connection to database.\n";
-		}		
-	
+		}
+
+	}
+
+	@Override
+	public void update() {
+		// TODO Automatisch gegenereerde methodestub
+
 	}
 }
