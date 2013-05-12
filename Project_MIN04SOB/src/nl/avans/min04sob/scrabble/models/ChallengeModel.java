1616925283e6b96ec68d3286ddc432bbca537960
waaghals <patrick.stroef@gmail.com>
2013-05-12 21:55:26 +0200

    Model stubs toegevoegd

---
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/ChallengeModel.java
@@ -0,0 +1,13 @@
+package nl.avans.min04sob.scrabble.models;
+
+import nl.avans.min04sob.scrabble.core.CoreModel;
+
+public class ChallengeModel extends CoreModel {
+
+	@Override
+	public void update() {
+		// TODO Automatisch gegenereerde methodestub
+
+	}
+
+}
---
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/GameModel.java
@@ -0,0 +1,13 @@
+package nl.avans.min04sob.scrabble.models;
+
+import nl.avans.min04sob.scrabble.core.CoreModel;
+
+public class GameModel extends CoreModel {
+
+	@Override
+	public void update() {
+		// TODO Automatisch gegenereerde methodestub
+
+	}
+
+}
