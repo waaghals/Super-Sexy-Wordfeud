1e67bbda21a480c429b7ad1a94a268e02f3171c0
waaghals <patrick.stroef@gmail.com>
2013-05-16 10:28:16 +0200

    Start the main GUI in a seperate thread

--- Project_MIN04SOB/src/testmains/TestMain.java
+++ Project_MIN04SOB/src/testmains/TestMain.java
@@ -1,16 +1,22 @@
 package testmains;
 
+import javax.swing.SwingUtilities;
+
 import nl.avans.min04sob.scrabble.controllers.MainController;
 
 
 
-public class TestMain {
+public class TestMain implements Runnable{
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		new Thread(new MainController()).start();
+	}
+
+	@Override
+	public void run() {
 		new MainController();
 	}
-//test
 }
