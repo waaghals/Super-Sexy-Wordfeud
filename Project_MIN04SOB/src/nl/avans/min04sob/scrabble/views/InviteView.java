c10b740300b6bd64e2b9ec6b96de640a9f989544
Thomas de Brouwer <tjm.debrouwer@student.avans.nl>
2013-05-21 12:38:04 +0200

    Gui voor InviteView gemaakt

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/InviteView.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/InviteView.java
@@ -8,10 +8,12 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import net.miginfocom.swing.MigLayout;
 import nl.avans.min04sob.scrabble.core.CorePanel;
+import javax.swing.JList;
 
 public class InviteView extends CorePanel{
-	
+	/*
 	private JScrollPane competitionScroll;
 	private JScrollPane allCompetitionScroll;
 	private JScrollPane competitionPlayersScroll;
@@ -22,8 +24,39 @@ public class InviteView extends CorePanel{
 	private JButton inviteButton;
 	private JButton joinButton;
 	private JLabel username;
-	
+	*/
 	public InviteView(){
+		setLayout(new MigLayout("", "[100px:100px:100px,grow][100px:142.00px:100px,grow][100px:100px:100px]", "[100px:100px:25px,grow][100px:100px:100px,grow][100px:150px:100px,grow][100px:100px:25px]"));
+		
+		JLabel lblUsername = new JLabel("Logged in as:");
+		add(lblUsername, "cell 0 0,alignx left,aligny top");
+		
+		JButton btnBack = new JButton("Back");
+		add(btnBack, "cell 2 0,alignx right,aligny top");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, "cell 0 1,grow");
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		add(scrollPane_2, "cell 1 1 2 2,grow");
+		
+		JList list_2 = new JList();
+		scrollPane_2.setViewportView(list_2);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		add(scrollPane_1, "cell 0 2 1 2,grow");
+		
+		JList list_1 = new JList();
+		scrollPane_1.setViewportView(list_1);
+		
+		JButton btnInvite = new JButton("Invite");
+		add(btnInvite, "cell 1 3,growx,aligny top");
+		
+		JButton btnJoin = new JButton("Join");
+		add(btnJoin, "cell 2 3,growx,aligny top");
 		
 	}
 
