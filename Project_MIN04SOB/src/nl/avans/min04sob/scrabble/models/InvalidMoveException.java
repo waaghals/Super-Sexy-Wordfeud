0f0739d36e93d667df1d4eea9ee43e455e2dfd41
waaghals <patrick.stroef@gmail.com>
2013-05-21 22:36:13 +0200

    Created a Exception for errorhandling in the app

---
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/InvalidMoveException.java
@@ -0,0 +1,21 @@
+package nl.avans.min04sob.scrabble.models;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class InvalidMoveException extends Exception {
+	public static final int NOT_ALIGNED = 1;
+	public static final int NOT_CONNECTED = 2;
+	public static final int NOT_ON_START = 3;
+	public static final int NO_LETTERS_PUT = 4;
+
+	private int errorType;
+
+	public InvalidMoveException(int error) {
+		if (ArrayUtils.contains(new int[] { NOT_ALIGNED, NOT_CONNECTED,
+				NOT_ON_START, NO_LETTERS_PUT }, error)) {
+			errorType = error;
+			return;
+		}
+		throw new IllegalArgumentException("Not a valid error value for InvalidMoveException");
+	}
+}
