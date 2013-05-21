60ab986091cdfdf93d9a0ae1877a4d1ccc459056
Tiancheng <Tiancheng@145.102.94.26>
2013-05-21 14:11:46 +0200

    niks te zoeken hier

---
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/ChallengeController.java
@@ -0,0 +1,52 @@
+package nl.avans.min04sob.scrabble.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import nl.avans.min04sob.scrabble.core.CoreController;
+import nl.avans.min04sob.scrabble.models.ChallengeModel;
+import nl.avans.min04sob.scrabble.views.ChallengeView;
+
+public class ChallengeController extends CoreController{
+	private ChallengeView cv = new ChallengeView();
+	private ChallengeModel  cm = new ChallengeModel();
+	
+	public ChallengeController ()
+	{
+		addView(cv);
+		addModel(cm);
+		
+		
+		 
+		cv.addActionListenerAccept (new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//dpeiets
+			}
+		});
+		cv.addActionListenerDecline (new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//dpeiets
+			}
+		});
+		cv.addActionListenerOke (new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//dpeiets
+			}
+		});
+		 
+ }
+
+	@Override
+	public void initialize() 
+	{
+		 
+		
+	}
+
+	@Override
+	public void addListeners() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/ChallengeView.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/ChallengeView.java
@@ -1,64 +1,116 @@
 package nl.avans.min04sob.scrabble.views;
 
- 
-	import java.awt.BorderLayout;
-	import java.awt.Dimension;
-	import java.awt.Font;
-	import java.awt.LayoutManager;
-	import java.awt.event.ActionEvent;
-	import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
 
-	import javax.swing.BoxLayout;
-	import javax.swing.JButton;
-	import javax.swing.JFrame;
-	import javax.swing.JLabel;
-	import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-	public class ChallengeView extends JFrame{
-//test
+import nl.avans.min04sob.scrabble.core.CoreController;
+import nl.avans.min04sob.scrabble.core.CorePanel;
+import nl.avans.min04sob.scrabble.core.CoreView;
+	public class ChallengeView   implements CoreView{
+		// extends??
+		private JTextField Opponent;
 		private JPanel chpanel = new JPanel();
+		private JPanel tochallenge = new JPanel();
+		private JPanel response=new JPanel();
 		private JButton accept = new JButton("accept");
 		private JButton decline = new JButton("decline");
-		
+		private JButton oke = new JButton("oke");
+		private JFrame jf = new JFrame("popup");
+		private JTextField usernameField = new JTextField(10);
+			
 		public ChallengeView()
 		{
-			
-			this.setTitle("Challenge");
-			setVisible(true);
-			showChallenge();
-			setResizable(false);
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			pack();
+			jf.setTitle("Challenge");
+			jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			jf.pack();
 		}
 		
-		public void showChallenge()
+		public void showChallenge()//gui 
 		{
-			this.setContentPane(chpanel);
+			jf.setResizable(false);
+			jf.setVisible(true);
+			jf.setContentPane(chpanel);
 			chpanel.setPreferredSize(new Dimension(180,70));
+			/*/
 			accept.addActionListener(new ActionListener(){
-
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
-					dispose();
-					// TODO Auto-generated method stub
-					
+					jf.dispose();
 				}});
 			decline.addActionListener(new ActionListener(){
 				@Override
 				public void actionPerformed(ActionEvent arg0) 
 				{
-					dispose();
-				}});
-		 
-			JLabel text = new JLabel("You have received a challenge");
-		
-			chpanel.add( text);
+					jf.dispose();
+				}}); 
+				/*/
+			chpanel.add( new JLabel("You have received a challenge") );
 			chpanel.add(accept);
 			chpanel.add(decline);
 
 			accept.setFont(new Font("Serif", Font.ITALIC, 14));
 			decline.setFont(new Font("Serif", Font.ITALIC, 14));
 		}
+		
+		public void toChallenge()//gui
+		{	
+			jf.setVisible(true);
+			jf .setContentPane(tochallenge);
+			tochallenge.setPreferredSize(new Dimension(180,70));
+			tochallenge.add(usernameField);
+			jf.add(oke);
+			/*/
+			oke.addActionListener(new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					/// controller
+					System.out.println(usernameField.getText());
+				}});
+				/*/
+		}
+		
+		public void response(String msg)//gui
+		{
+			jf.setResizable(false);
+			jf.setVisible(true);
+			jf.setContentPane(response);
+			response.setPreferredSize(new Dimension(130,70));
+			response.add(new JLabel("msg"));
+			response.add(oke);
+		}
+		
+		public String getUsername() 
+		{
+			return usernameField.getText();
+		}
+
+		 
+
+		@Override
+		public void modelPropertyChange(PropertyChangeEvent evt) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		public void addActionListenerAccept(ActionListener listener) {
+			accept.addActionListener(listener);
+		}
+		public void addActionListenerOke(ActionListener listener) {
+			oke.addActionListener(listener);
+		}
+		public void addActionListenerDecline(ActionListener listener) {
+			decline.addActionListener(listener);
+		}
 	}
-	
-	// test
