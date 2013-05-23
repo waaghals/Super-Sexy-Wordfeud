dc76b029beb508776ed90d90feb9a032d1dd9620
Thomas de Brouwer <tjm.debrouwer@student.avans.nl>
2013-05-23 19:59:00 +0200

    paar toevoegingen gedaan, buttonListeners aangemaakt en iets in gezet(waarschijnlijk nog niet correct)

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/InviteView.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/InviteView.java
@@ -14,8 +14,8 @@ import javax.swing.JList;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class InviteView extends CorePanel{
-	
+public class InviteView extends CorePanel {
+
 	private JButton actieButton;
 	private JButton annuleerButton;
 	private JLabel competitionLabel;
@@ -23,53 +23,57 @@ public class InviteView extends CorePanel{
 	private String competitionLabelText;
 	private String spelersLabelText;
 	private String actieButtonText;
-	
-	public InviteView(){
-		setLayout(new MigLayout("", "[100px:120px:120px,grow][100px:142.00px:100px,grow][100px:100px:100px]", "[][100px:100px:100px,grow][][100px:150px:100px,grow][100px:100px:25px]"));
-		
-		competitionLabel = new JLabel(competitionLabelText); //aanpassen naar variabelle
+
+	public InviteView() {
+		setLayout(new MigLayout(
+				"",
+				"[100px:120px:120px,grow][100px:142.00px:100px,grow][100px:100px:100px]",
+				"[][100px:100px:100px,grow][][100px:150px:100px,grow][100px:100px:25px]"));
+
+		competitionLabel = new JLabel(competitionLabelText); // aanpassen naar
+																// variabelle
 		add(competitionLabel, "cell 0 0");
-		
-		spelersLabel = new JLabel(spelersLabelText); //aanpassen naar variabelle
+
+		spelersLabel = new JLabel(spelersLabelText); // aanpassen naar
+														// variabelle
 		add(spelersLabel, "cell 1 0");
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane, "cell 0 1 1 3,grow");
-		
+
 		JList competitionList = new JList();
 		scrollPane.setViewportView(competitionList);
-		
+
 		JScrollPane scrollPane_2 = new JScrollPane();
 		add(scrollPane_2, "cell 1 1 2 3,grow");
-		
+
 		JList playerList = new JList();
 		scrollPane_2.setViewportView(playerList);
-		
+
 		JButton annuleerButton = new JButton("Annuleer");
-		annuleerButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		add(annuleerButton, "cell 0 4,alignx left,aligny top");
-		
-		JButton actieButton = new JButton(actieButtonText); //aanpassen naar variabelle
-		actieButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
+
+		JButton actieButton = new JButton(actieButtonText); // aanpassen naar
+															// variabelle
 		add(actieButton, "cell 2 4,growx,aligny top");
 	}
-	
-	public void addActionListenerActieButton(ActionListener listener){
+
+	public void addActionListenerActieButton(ActionListener listener) {
 		actieButton.addActionListener(listener);
 	}
+
+	public void addActionListenerAnnuleerButton(ActionListener listener) {
+		annuleerButton.addActionListener(listener);
+	}
+
 	@Override
 	public void modelPropertyChange(PropertyChangeEvent evt) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	public void setButtons(String competitionLabelText, String spelersLabelText, String actieButtonText){
+
+	public void setButtons(String competitionLabelText,
+			String spelersLabelText, String actieButtonText) {
 		this.competitionLabelText = competitionLabelText;
 		this.spelersLabelText = spelersLabelText;
 		this.actieButtonText = actieButtonText;
