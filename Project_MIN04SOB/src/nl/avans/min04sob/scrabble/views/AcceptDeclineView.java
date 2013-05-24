d4bea6b9c46a0336b64b6129b7fd7427c174c2c5
luuk <luuk-de-bruin@hotmail.com>
2013-05-24 13:30:13 +0200

    aanpassing acceptdeclineview

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/AcceptDeclineView.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/AcceptDeclineView.java
@@ -1,16 +1,33 @@
 package nl.avans.min04sob.scrabble.views;
 
+import java.beans.PropertyChangeEvent;
+
+import javax.swing.JFrame;
 import javax.swing.JList;
 
+import net.miginfocom.swing.MigLayout;
+import nl.avans.min04sob.scrabble.core.CorePanel;
 import nl.avans.min04sob.scrabble.models.GameModel;
 
-public class AcceptDeclineView {
+public class AcceptDeclineView extends CorePanel{
 
 	private JList wordList;
 	private GameModel gameModel = new GameModel();
+	private JFrame myFrame;
 	
 	public AcceptDeclineView()
 	{
+		setLayout(new MigLayout(
+				"",
+				"[100px:120px:120px,grow][100px:142.00px:100px,grow][100px:100px:100px]",
+				"[][100px:100px:100px,grow][][100px:150px:100px,grow][100px:100px:25px]"));
 		wordList = new JList(gameModel.getRequestedWords());
+		
+	}
+
+	@Override
+	public void modelPropertyChange(PropertyChangeEvent evt) {
+		// TODO Auto-generated method stub
+		
 	}
 }
