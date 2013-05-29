f4f10f5b0745800a4242e171fd006c2693e3714b
Alexander van Doorn <turboblaster@live.nl>
2013-05-29 12:51:50 +0200

    addresignbuttonlistener naar addbuttonlisteners

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/MainController.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/MainController.java
@@ -208,7 +208,7 @@ public class MainController extends CoreController {
 	}
 
 
-	private void addResignButtonListener() {
+	private void addButtonListeners() {
 		currGamePanel.addResignActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -297,7 +297,7 @@ public class MainController extends CoreController {
 		selectedGame.getBoardFromDatabase();
 		selectedGame.update();
 
-		addResignButtonListener();
+		addButtonListeners();
 
 		frame.getContentPane().add(currGamePanel, "cell 4 0 6 7,grow");
 		frame.revalidate();
