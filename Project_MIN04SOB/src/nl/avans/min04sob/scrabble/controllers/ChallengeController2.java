8b3b8639e43b615c5fcd3cb460deb4cf3c5094e4
Alexander van Doorn <turboblaster@live.nl>
2013-05-29 18:27:36 +0200

    resignController netter gemaakt, challenge2dingen gemaakt, alleen model hiervoor nog niet

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/ChallengeController2.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/ChallengeController2.java
@@ -1,20 +1,79 @@
 package nl.avans.min04sob.scrabble.controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
 import nl.avans.min04sob.scrabble.core.CoreController;
+import nl.avans.min04sob.scrabble.models.ChallengeModel;
+import nl.avans.min04sob.scrabble.views.ChallengeView2;
 
 
 public class ChallengeController2 extends CoreController {
 
+	private ChallengeView2 challengeView2;
+	private ChallengeModel challengeModel;
+	private JFrame frame;
+	
+	public ChallengeController2() {
+		initialize();
+		addListeners();
+		
+		frame.setAlwaysOnTop(true);
+		frame.add(challengeView2);
+		
+		addView(challengeView2);
+		addModel(challengeModel);
+		
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
-		
+		frame = new JFrame();
+		challengeView2 = new ChallengeView2();
+		challengeModel = new ChallengeModel();
 	}
 
 	@Override
 	public void addListeners() {
-		// TODO Auto-generated method stub
+		challengeView2.addActionListenerAccept(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				acceptChallenge();
+			}
+		});
+		challengeView2.addActionListenerDecline(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				declineChallenge();
+			}
+		});
+		challengeView2.addActionListenerBack(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				goBack();
+			}
+		});
 		
+	}
+	
+	private void acceptChallenge() {
+		//TODO doesnt have a good model yet
+		//challengeModel.something();
+	}
+	
+	private void declineChallenge() {
+		//TODO RT acceptchallenge()
+	}
+	
+	private void goBack() {
+		frame.dispose();
+		frame = null;
 	}
 
 }
--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/ChallengeView2.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/views/ChallengeView2.java
@@ -1,5 +1,6 @@
 package nl.avans.min04sob.scrabble.views;
 
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 
 import nl.avans.min04sob.scrabble.core.CorePanel;
@@ -13,28 +14,58 @@ import javax.swing.JComboBox;
 import net.miginfocom.swing.MigLayout;
 
 public class ChallengeView2 extends CorePanel {
+
+	private JButton acceptButton;
+	private JButton declineButton;
+	private JButton backButton;
+	private JLabel challengeLabel;
+	private JScrollPane scrollPane;
+	private JList challengeList;
+
+	
+	
 	public ChallengeView2() {
 		setLayout(new MigLayout("", "[150px:150px:150px,grow][100px:100px:100px,grow]", "[14px][40px:40px:40px][40px:40px:40px,grow][grow][40px:40px:40px,grow]"));
 		
-		JLabel challengeLabel = new JLabel("Uitdagingen");
+		challengeLabel = new JLabel("Uitdagingen");
 		add(challengeLabel, "cell 0 0 2 1,alignx left,aligny top");
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		add(scrollPane, "cell 0 1 1 3,grow");
 		
-		JList challengeList = new JList();
+		challengeList = new JList();
 		scrollPane.setViewportView(challengeList);
 		
-		JButton acceptButton = new JButton("Accepteer");
+		acceptButton = new JButton("Accepteer");
 		add(acceptButton, "cell 1 1,alignx center");
 		
-		JButton declineButton = new JButton("Negeer");
+		declineButton = new JButton("Negeer");
 		add(declineButton, "cell 1 2,alignx center");
 		
-		JButton backButton = new JButton("Ga terug");
+		backButton = new JButton("Ga terug");
 		add(backButton, "cell 0 4");
 	}
 
+	public void addActionListenerAccept(ActionListener listener) {
+		acceptButton.addActionListener(listener);
+	}
+	
+	public void addActionListenerDecline(ActionListener listener) {
+		declineButton.addActionListener(listener);
+	}
+	
+	public void addActionListenerBack(ActionListener listener) {
+		backButton.addActionListener(listener);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@Override
 	public void modelPropertyChange(PropertyChangeEvent evt) {
 		// TODO Auto-generated method stub
