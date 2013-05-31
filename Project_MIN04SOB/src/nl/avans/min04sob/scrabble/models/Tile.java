a3e382a08c870a9c69522e3c870214d9711f15da
waaghals <patrick.stroef@gmail.com>
2013-05-31 19:33:35 +0200

    Drag and drop almost bug free

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/Tile.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/models/Tile.java
@@ -1,27 +1,34 @@
 package nl.avans.min04sob.scrabble.models;
 
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 
-public class Tile {
+public class Tile implements Transferable {
 	private String letter;
 	private int value;
 	private boolean mutatable;
 	public final static boolean MUTATABLE = true;
 	public final static boolean NOT_MUTATABLE = false;
-	
-	
-	public Tile(){
+	private final static DataFlavor flavors[] = { new DataFlavor(Tile.class,
+			"Tile") };
+
+	public Tile() {
 		letter = "";
 		value = 0;
 		mutatable = true;
 	}
-	
-	@Deprecated public Tile(String character, boolean mutable){
+
+	@Deprecated
+	public Tile(String character, boolean mutable) {
 		letter = character;
 		value = 0;
 		this.mutatable = mutable;
 	}
-	
-	@Deprecated public Tile(String character, int charValue) {
+
+	@Deprecated
+	public Tile(String character, int charValue) {
 		letter = character;
 		value = charValue;
 		mutatable = true;
@@ -34,15 +41,14 @@ public class Tile {
 	}
 
 	public String getLetter() {
-		if(!isEmpty()){  
+		if (!isEmpty()) {
 			return letter;
-		}
-		else{
+		} else {
 			return "";
 		}
 	}
 
-	public int getValue(){
+	public int getValue() {
 		return value;
 	}
 
@@ -50,28 +56,50 @@ public class Tile {
 		return letter.equals("");
 	}
 
-	public boolean isMutatable(){
+	public boolean isMutatable() {
 		return mutatable;
 	}
-	
-	public void lock(){
+
+	public void lock() {
 		mutatable = false;
 	}
-	
+
 	public void setEmpty(boolean empty) {
-		if(empty){
+		if (empty) {
 			letter = null;
 		}
 	}
-	
+
 	public void setLetter(String letter) {
 		this.letter = letter;
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		return letter;
 	}
-	
-	
+
+	@Override
+	public Object getTransferData(DataFlavor flavor) {
+		if (isDataFlavorSupported(flavor)) {
+			return this;
+		}
+		return null;
+	}
+
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		return flavors;
+	}
+
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor value) {
+		for (DataFlavor flavor : flavors) {
+			if (flavor == value || value != null && value.equals(flavor)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
