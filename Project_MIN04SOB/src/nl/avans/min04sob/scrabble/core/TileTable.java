edc7a8f410d1b2c998eb27ee8fd0a051d25c9aca
waaghals <patrick.stroef@gmail.com>
2013-05-31 18:56:56 +0200

    Added multiplier text to tooltip

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/core/TileTable.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/core/TileTable.java
@@ -5,22 +5,48 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JTable;
 
+import nl.avans.min04sob.scrabble.models.BoardModel;
 import nl.avans.min04sob.scrabble.models.Tile;
 
 public class TileTable extends JTable {
+	private StringBuilder toolTip;
+
 	@Override
 	public String getToolTipText(MouseEvent event) {
+		toolTip =  new StringBuilder();
 		Point p = event.getPoint();
 		// Locate the renderer under the event location
 		int colIndex = columnAtPoint(p);
 		int rowIndex = rowAtPoint(p);
-		
-		Tile tile = (Tile) getModel().getValueAt(rowIndex, colIndex);
-		if (tile != null) {
-			return "Waarde: " + tile.getValue();
+
+		BoardModel model = (BoardModel) getModel();
+		Tile tile = (Tile) model.getValueAt(rowIndex, colIndex);
+
+		int multiplier = model.getMultiplier(new Point(rowIndex, colIndex));
+		switch (multiplier) {
+		case BoardModel.DL:
+			toolTip.append("(DL) ");
+			break;
+		case BoardModel.TL:
+			toolTip.append("(TL) ");
+			break;
+		case BoardModel.DW:
+			toolTip.append("(DW) ");
+			break;
+		case BoardModel.TW:
+			toolTip.append("(TW) ");
+			break;
 		}
 
-		return "";
+		if (tile != null) {
+			toolTip.append("Waarde:" + tile.getValue());
+		}
+
+		if(toolTip.length() == 0){
+			return null;
+		}
 		
+		return toolTip.toString();
+
 	}
 }
