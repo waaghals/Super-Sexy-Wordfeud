dd0de43291bc13cad26570aa001a35343dc0fdb5
Alexander van Doorn <turboblaster@live.nl>
2013-06-03 15:30:12 +0200

    preferredsize gezet voor mainscherm

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/MainController.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/MainController.java
@@ -1,6 +1,7 @@
 
 package nl.avans.min04sob.scrabble.controllers;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -60,7 +61,7 @@ public class MainController extends CoreController {
 		// }
 
 		frame.setJMenuBar(menu);
-
+		frame.setPreferredSize(new Dimension(1000,680));
 		frame.pack();
 	}
 
