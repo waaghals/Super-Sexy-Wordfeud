7198391a5eb2e7b42ce42452825403acfccbebd1
Tiancheng <Tiancheng@192.168.1.108>
2013-06-04 12:59:32 +0200

    nu kan die niet kutte als je niks selecteerd

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/AccountController.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/controllers/AccountController.java
@@ -58,9 +58,14 @@ public class AccountController extends CoreController {
 		changepassPaneladmin.ChangeaddActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				 System.out.println(changepassPaneladmin.getSelectedPlayer());
+				 if(changepassPaneladmin.getSelectedPlayer()!=null){
 				 accountModel.changeAnotherPlayerPass(changepassPaneladmin.getSelectedNewPass(),changepassPaneladmin.getSelectedPlayer());
 				 frame.dispose();
+				 }
+				 else
+				 {
+					 changepassPaneladmin.setText("You need to select first");
+				 }
 			}
 		});
 		
