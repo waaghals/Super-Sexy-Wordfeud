b21f43961c74b0674e71a3e7af11f39b354d0700
waaghals <patrick.stroef@gmail.com>
2013-06-05 20:08:04 +0200

    Swag and Drop (TM) done, almost bug free, but that is Swing's fault. You could say swing is kind of a drag.....

--- Project_MIN04SOB/src/nl/avans/min04sob/scrabble/misc/TileTranfserHandler.java
+++ Project_MIN04SOB/src/nl/avans/min04sob/scrabble/misc/TileTranfserHandler.java
@@ -1,4 +1,3 @@
-
 package nl.avans.min04sob.scrabble.misc;
 
 import java.awt.Point;
@@ -32,24 +31,25 @@ public class TileTranfserHandler extends TransferHandler {
 				System.out.println("Source tile not mutable or null");
 				return false;
 			}
-			//support.setShowDropLocation(true);
+			// support.setShowDropLocation(true);
 			JTable table = (JTable) support.getComponent();
 			TableModel model = table.getModel();
 			int row = table.getSelectedRow();
 			int col = table.getSelectedColumn();
 			Tile tile = (Tile) model.getValueAt(row, col);
-			
+
 			if (tile == null) {
 				System.out.println("Drop loc. is valid because it is null");
 				return true;
 			}
 
 			if (tile.equals(sourceTile)) {
-				System.out.println("Drop loc. is valid because it is the same as the source dest");
+				System.out
+						.println("Drop loc. is valid because it is the same as the source dest");
 				return true;
 			}
 		} catch (UnsupportedFlavorException | IOException e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
 
 		return false;
@@ -63,36 +63,42 @@ public class TileTranfserHandler extends TransferHandler {
 		int col = sourceTable.getSelectedColumn();
 
 		Tile sourceTile = (Tile) sourceTable.getModel().getValueAt(row, col);
-		return sourceTile; 
+		return sourceTile;
 	}
 
 	@Override
 	protected void exportDone(JComponent source, Transferable data, int action) {
 		Tile sourceTile;
-	
+		try {
 			JTable table = (JTable) source;
 			int sourceRow = table.getSelectedRow();
 			int sourceCol = table.getSelectedColumn();
 			TableModel model = table.getModel();
-			//sourceTile = (Tile) data.getTransferData(tileFlavor);
-			//sourceTile = (Tile) model.getValueAt(sourceRow, sourceCol);
-			//if (sourceTile != null && sourceTile.isMutatable()) {
-				System.out.println("Setting value at: R" + sourceRow + " C" + sourceCol + " to null");
-				model.setValueAt(null, sourceRow, sourceCol);
-			//}
 
+			sourceTile = (Tile) data.getTransferData(tileFlavor);
+			if (sourceTile != null && sourceTile.isMutatable()) {
+				System.out.println("Setting value at: R" + sourceRow + " C"
+						+ sourceCol + " to null");
+				model.setValueAt(null, sourceRow, sourceCol);
+			} else {
+				//Put the sourceTile Back
+				model.setValueAt(sourceTile, sourceRow, sourceCol);
+			}
 			table.clearSelection();
-	
+		} catch (UnsupportedFlavorException | IOException e) {
+			// TODO Automatisch gegenereerd catch-blok
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public int getSourceActions(JComponent c) {
 		return MOVE;
 	}
-	
+
 	@Override
 	public boolean importData(TransferSupport support) {
-		if(!canImport(support)){
+		if (!canImport(support)) {
 			return false;
 		}
 		JTable table = (JTable) support.getComponent();
